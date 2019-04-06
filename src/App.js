f75ad9073103b7a9fe6fe944a8042c209b9b6aea
pankaj <pankaj.007n@gmail.com>
2019-04-06 22:09:35 +0530

    IC

--- src/App.js
+++ src/App.js
@@ -3,23 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    feeds: [],
+    image: '',
+    title: ''
+  }
+
+  inputChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  postFeed = () => {
+    const {image, title} = this.state;
+    if(!image || !title) {
+      return;
+    }
+    this.setState({
+      feeds: [
+        ...this.state.feeds,
+        {
+          image,
+          title
+        }
+      ],
+      image: '',
+      title: ''
+    });
+  }
+
   render() {
+    const { feeds } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="post-form">
+          <input value={this.state.image} onChange={this.inputChange} name="image" type="text" placeholder="Enter URL" />
+          <input value={this.state.title} onChange={this.inputChange} name="title" type="text" placeholder="Title" />
+          <button onClick={this.postFeed} name="post">
+            New Post
+          </button>
+        </div>
+        <ul className="feed-list">
+          {
+            feeds.map((feed, index) => (
+              <li key={index} className="post">
+                <div>{feed.title}</div>
+                <img src={feed.image} alt={feed.title} />
+              </li>
+            ))
+          }
+        </ul>
       </div>
     );
   }
